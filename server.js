97cd26e04f4f96da44dcf5f8ce139d68876c7b83
bradragz <ragonesebradley@gmail.com>
2024-08-12 19:48:48 -0700

    create express with middleware, public directory access with static, and routing to html/api routes

--- server.js
+++ server.js
@@ -3,4 +3,26 @@ const express = require('express');
 const path = require('path');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+const { prototype } = require('events');
 
+// init express app and define port
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// middleware for JSON and URL-encoded data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// middleware to allow the publice directory to be accessed directly via browser
+app.use(express.static('public'));
+
+// setting up the routes for API requests using apiRoutes
+app.use('/api', apiRoutes);
+
+// setting up the routes for the html requests using htmlRoutes
+app.use('/', htmlRoutes);
+
+// start server and give console log that the server is listening and on the port it is listneing
+app.listen(PORT, () =>{
+    console.log(`You are now connected! Server is listening on port ${PORT}`)
+})
