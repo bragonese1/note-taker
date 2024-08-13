55a2f07b0d4ff9282411e7e9b5ae199763e1e40d
bradragz <ragonesebradley@gmail.com>
2024-08-12 21:30:50 -0700

    adding post route to create and save notes with IDs

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,4 +11,20 @@ router.get('/notes', (req, res) => {
         res.json(JSON.parse(data));
     });
 });
+// POST request for adding a new note
+router.post('/notes', (req, res) => {
+    // Creating a new note and assign a unique ID to it
+    const newNote = { id: uuidv4(), ...req.body };
+    // Reading the existing notes from the db.json file
+    fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err,
+        data) => {
+            if (err) throw err;
+            const notes = JSON.parse(data);
+            notes.push(newNote);
+            fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notes), (err) => {
+                if (err) throw err;
+                res.json(newNote);
+            });
+        });
+});
 
