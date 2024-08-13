ef8564665692cace944113860ff2283c790f92e4
bradragz <ragonesebradley@gmail.com>
2024-08-12 18:53:00 -0700

    creating file structure for routes/creating required modules

---
+++ server.js
@@ -0,0 +1,6 @@
+//load required modules
+const express = require('express');
+const path = require('path');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
