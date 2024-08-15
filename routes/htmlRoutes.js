517d7a0cdcc0e989d2b8d06de2033530be26b16d
bradragz <ragonesebradley@gmail.com>
2024-08-14 19:15:34 -0700

    changing / to * in get to match assignment

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -9,8 +9,8 @@ router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
-// Root route for the index.html
-router.get('/', (req, res) => {
+// Route for the index.html
+router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
